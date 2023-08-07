479d5b4b83c9a11a7447c160dcc337b08dfb7892
krs9891 <krzysztof.russek@protonmail.com>
2023-08-07 20:25:32 +0200

    transfer.sh script logic developed

--- bash-keep_sh-file-transfer/transfer.sh
+++ bash-keep_sh-file-transfer/transfer.sh
@@ -3,36 +3,70 @@
 currentVersion="0.0.1"
 
 show_help() {
-    echo "Description: Bash tool to transfer files from the command line."
-    echo "Usage:"
-    echo "  -d <directory> <file_id> <file_name>    Download a single file from transfer.sh to the specified directory."
-    echo "  -h                                     Show this help message."
-    echo "  -v                                     Get the tool version."
-    echo "Examples:"
-    echo "  ./transfer.sh test.txt ..."
+    printf "  %-20s %s\n" "Description:" "Bash tool to transfer files from the command line."
+    printf "Usage:"
+    printf "  %-20s %s\n" "-d <directory> <file_id> <file_name>" "Download a single file from transfer.sh to the specified directory."
+    printf "  %-20s %s\n" "-h" "Show this help message."
+    printf "  %-20s %s\n" "-v" "Get the tool version."
+    printf "  %-20s %s\n" "Examples:" "./transfer.sh test.txt ..."
 }
 
 show_version() {
     echo $currentVersion
 }
 
-upload_files() {
-    echo "Uploading files: $@"
+httpSingleUpload() {
+    response=$(curl --upload-file "$1" "https://free.keep.sh") || {
+        echo "Failure!"
+        return 1
+    }
 }
 
-download_file() {
-    local directory="$1"
-    local file_id="$2"
-    local file_name="$3"
-    echo "Downloading file with ID: $file_id to directory: $directory with name: $file_name"
+printUploadResponse() {
+    file_link=$(echo "$response" | grep -o 'https://free.keep.sh/[^[:space:]]*')
+    echo "$response" | sed '$d'
+    echo "Transfer File URL: $file_link"
 }
 
-# Parse flags using getopts
+singleUpload() {
+    filePath=${1//\~/$HOME}
+    if [ ! -f "$filePath" ]; then {
+        echo "Error: invalid file path"
+        return 1
+    }; fi
+    tempFileName=$(echo "$1" | sed "s/.*\///")
+    echo "Uploading $tempFileName"
+    httpSingleUpload "$filePath"
+}
+
+singleDownload() {
+    directory="$1"
+    file_id="$2"
+    file_name="$3"
+
+    if [ ! -d "$directory" ]; then
+        mkdir -p "$directory" || {
+            echo "Error: Failed to create directory $directory"
+            return 1
+        }
+    fi
+
+    echo "Downloading $file_name"
+    response=$(curl -L "https://free.keep.sh/$file_id/$file_name" >"$directory/$file_name") || {
+        echo "Failure!"
+        return 1
+    }
+}
+
+printDownloadResponse() {
+    echo "$response"
+    echo "Success!"
+}
+
 while getopts ":d:hv" opt; do
     case $opt in
     d)
         directory="$OPTARG"
-        shift $((OPTIND - 1))
         ;;
     h)
         show_help
@@ -43,26 +77,33 @@ while getopts ":d:hv" opt; do
         exit 0
         ;;
     \?)
-        echo "Invalid option: -$OPTARG" >&2
+        echo "Invalid option: -$OPTARG"
         exit 1
         ;;
     esac
 done
 
-# Shift to the next argument after flags
 shift $((OPTIND - 1))
 
 # Check if -d flag is provided to trigger the download
-if [[ -n "$directory" && $# -eq 2 ]]; then
-    file_id="$1"
-    file_name="$2"
-    download_file "$directory" "$file_id" "$file_name"
+if [[ -n "$directory" ]]; then
+    if [[ $# -eq 2 ]]; then
+        file_id="$1"
+        file_name="$2"
+        singleDownload "$directory" "$file_id" "$file_name"
+        printDownloadResponse
+    else
+        echo "Invalid number of arguments for -d flag."
+        exit 1
+    fi
 elif [[ $# -gt 0 ]]; then
     # If no -d flag, assume it's an upload operation
-    upload_files "$@"
+    for file in "$@"; do
+        singleUpload "$file" || exit 1
+        printUploadResponse
+    done
 else
     # If no flags or arguments provided, show help message
-    echo "Invalid number of arguments for the download operation."
     show_help
     exit 1
 fi
